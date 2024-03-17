f4defbaa112e8e359c9e835060c60d74b4ff8891
Faizan <faizanamjad575@gmail.com>
2024-03-17 19:19:42 +0500

    Create a 'book' object constructor

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 //Declarations
 ////////////////////////////////////
 var myLibrary = []; //array to store books
+
+function book(name, author, pages, read_status) {//A book object constructor
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read_status = read_status;
+    this.info = function(name, author, pages, read_status){
+        console.log(`${this.name}, by ${this.author}, ${this.pages} pages, ${this.read_status}.`);
+    }
+}
